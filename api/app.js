3406567baa6f522c4112500b84cae91cdb3efcad
Hongzhi Zhao (Ethan) <91398474+oberonbot@users.noreply.github.com>
2023-03-01 00:10:24 -0500

    modify

--- api/app.js
+++ api/app.js
@@ -61,7 +61,7 @@ app.use("/api/blog", blogRoutes);
 
 // start server
 const port =
-  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
+  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 8000;
 const server = app.listen(port, function () {
   console.log("Server listening on port " + port);
 });
